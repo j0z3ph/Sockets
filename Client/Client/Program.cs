5b183ff9d37442e7cebefd5c942f8e02ebadc3b6
Jose Luis Cruz <j0z3ph@gmail.com>
2022-12-08 08:26:57 -0600

    Add project files.

---
+++ Client/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client {
+    internal class Program {
+        static async Task Main(string[] args) {
+            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+            using Socket client = new(ipEndPoint.AddressFamily,
+                                    SocketType.Stream,
+                                    ProtocolType.Tcp);
+
+            await client.ConnectAsync(ipEndPoint);
+            while (true) {
+                // Send message.
+                var message = Console.ReadLine();
+                message = message.ToLower().Equals("bye") ? "<|BYE|>" : string.Concat(message, "<|EOM|>");
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                _ = await client.SendAsync(messageBytes, SocketFlags.None);
+                //Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+                // Receive ack.
+                var buffer = new byte[1_024];
+                var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                if (response == "<|BYE|>") {
+                    Console.WriteLine(
+                        $"Socket client received bye: \"{response}\"");
+                    break;
+                }
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server {
+    internal class Program {
+        static async Task Main(string[] args) {
+            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+            using Socket listener = new(ipEndPoint.AddressFamily,
+                                    SocketType.Stream,
+                                    ProtocolType.Tcp);
+
+            listener.Bind(ipEndPoint);
+            listener.Listen(100);
+
+            var handler = await listener.AcceptAsync();
+            while (true) {
+                // Receive message.
+                var buffer = new byte[1_024];
+                var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+                var eom = "<|EOM|>";
+                if (response.IndexOf(eom) > -1) {
+                    Console.WriteLine(
+                        $"Socket server received message: \"{response.Replace(eom, "")}\"");
+                    var aknMessage = "<|AKN|>";
+                    var echoBytes = Encoding.UTF8.GetBytes(aknMessage);
+                    await handler.SendAsync(echoBytes, 0);
+                }
+                if(response == "<|BYE|>") {
+                    var byeMessage = "<|BYE|>";
+                    var echoBytes = Encoding.UTF8.GetBytes(byeMessage);
+                    await handler.SendAsync(echoBytes, 0);
+                    Console.WriteLine(
+                        $"Socket server sent bye message: \"{byeMessage}\"");
+
+                    break;
+                }
+            }
+        }
+    }
+}
